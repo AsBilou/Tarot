d13248758b5bcb727c98085a10c69d1fb58fef8c
Quentin Mangin <quentin.mangin@gmail.com>
2013-01-30 19:24:53 +0100

    La fin est proche !!! :D

--- app/web.php
+++ app/web.php
@@ -1,9 +1,8 @@
 <?php
 /*
 *TODO :
-*	- Lors de la création d'un tournois, mettre les autre a false.
-*	- Si il n'y a pas de tournois, proposer un bouton pour en créer un.
-* 	- Afficher qu'il n'y a pas encore de game pour le tournois.
+*	- Faire le Cron
+*	- Voir pour le probleme de CSS
 */
 use Silex\Provider\FormServiceProvider;
 use Symfony\Component\HttpFoundation\Request;
@@ -22,51 +21,61 @@ $app->get('/', function () use ($app) {
     $tournaments = TournamentQuery::create()
         ->filterByActive(true)
         ->find();
-    //Recuperer les player du tournois
-    $player_tournament = TournamentPlayerQuery::create()
-        ->filterByTournamentId($tournaments[0]->getId())
-        ->find();
-    //Recupere les games du tournois
-    $games = GameQuery::create()
-        ->filterByTournamentId($tournaments[0]->getId())
-        ->find();
-    //recupere les player de la game
-    foreach($games as $game){
-        $gameId = $game->getId();
-        //recupération des joueurs
-        $player_game[$gameId] = GamePlayerQuery::create()
-        ->filterByGame($game)
-        ->find();
-    }
-    //Comparaison des deux
-    //Parcour de chaque game
-    foreach($games as $game){
-        $game_id=$game->getId();
-        $array_game['game_'.$game_id]['id_game'] = $game_id;
-        $game_bids = $game->getBids();
-        $array_game['game_'.$game_id]['bids'] = $game_bids;
-        $game_score = $game->getScore();
-        $array_game['game_'.$game_id]['score'] = $game_score;
-        foreach($player_tournament as $player){
-            $player_id_tournament = $player->getPlayerId();
-            $array_game['game_'.$game_id]['players'][$player_id_tournament]['score'] = 'OUT';
-            $array_game['game_'.$game_id]['players'][$player_id_tournament]['type'] = 'OUT';
-            foreach($player_game[$game->getId()] as $player){
-                 $player_id_game = $player->getPlayerId();
-                if($player_id_tournament == $player_id_game){
-                    $array_game['game_'.$game_id]['players'][$player_id_tournament]['score'] = $player->getScore();
-                    $array_game['game_'.$game_id]['players'][$player_id_tournament]['type'] = $player->getType();
-                }
-            }
-        }
-    }
+      
+    if(empty($tournaments[0])){
+    	$emptyTournament = true;
+    }else{
+    	$emptyTournament = false;
+    	//Recuperer les player du tournois
+    	$player_tournament = TournamentPlayerQuery::create()
+    	    ->filterByTournamentId($tournaments[0]->getId())
+    	    ->find();
+	    //Recupere les games du tournois
+	    $games = GameQuery::create()
+	        ->filterByTournamentId($tournaments[0]->getId())
+	        ->find();
+	    if(empty($games[0])){
+		     $emptyGames = true;   
+	    }else{
+		    $emptyGames = false;
+	    }
+	    //recupere les player de la game
+	    foreach($games as $game){
+	        $gameId = $game->getId();
+	        //recupération des joueurs
+	        $player_game[$gameId] = GamePlayerQuery::create()
+	        ->filterByGame($game)
+	        ->find();
+	    }
+	    //Comparaison des deux
+	    //Parcour de chaque game
+	    foreach($games as $game){
+	        $game_id=$game->getId();
+	        $array_game['game_'.$game_id]['id_game'] = $game_id;
+	        $game_bids = $game->getBids();
+	        $array_game['game_'.$game_id]['bids'] = $game_bids;
+	        $game_score = $game->getScore();
+	        $array_game['game_'.$game_id]['score'] = $game_score;
+	        foreach($player_tournament as $player){
+	            $player_id_tournament = $player->getPlayerId();
+	            $array_game['game_'.$game_id]['players'][$player_id_tournament]['score'] = 'OUT';
+	            $array_game['game_'.$game_id]['players'][$player_id_tournament]['type'] = 'OUT';
+	            foreach($player_game[$game->getId()] as $player){
+	                 $player_id_game = $player->getPlayerId();
+	                if($player_id_tournament == $player_id_game){
+	                    $array_game['game_'.$game_id]['players'][$player_id_tournament]['score'] = $player->getScore();
+	                    $array_game['game_'.$game_id]['players'][$player_id_tournament]['type'] = $player->getType();
+	                }
+	            }
+	        }
+	    }
 
-    foreach($player_tournament as $player_in_game){
-        $array_player['player_'.$player_in_game->getPlayerId()]['id']=$player_in_game->getPlayerId();
-        $array_player['player_'.$player_in_game->getPlayerId()]['score']=$player_in_game->getScore();
-        $array_player['player_'.$player_in_game->getPlayerId()]['name']=$player_in_game->getPlayer()->getName();
+	    foreach($player_tournament as $player_in_game){
+	        $array_player['player_'.$player_in_game->getPlayerId()]['id']=$player_in_game->getPlayerId();
+	        $array_player['player_'.$player_in_game->getPlayerId()]['score']=$player_in_game->getScore();
+	        $array_player['player_'.$player_in_game->getPlayerId()]['name']=$player_in_game->getPlayer()->getName();
+	    }
     }
-    
      /*
     echo '<pre>';
         print_r($array_game);
@@ -75,7 +84,8 @@ $app->get('/', function () use ($app) {
     return $app['twig']->render('template/index.twig', array(
         'array' => $array_game,
         'players' => $array_player,
-        'id_tournament'=>$tournaments[0]->getId()
+        'isTournament'=>$emptyTournament,
+        'isGame'=>$emptyGames
     ));
 
     //Récuperer tous les tournois
@@ -107,6 +117,15 @@ $tournaments = TournamentQuery::create()
 });
 
 $app->get('/tournament/create', function () use ($app) {
+	//Recupere les games du tournois
+    $tournaments = TournamentQuery::create()
+        ->find();
+    //recupere les player de la game
+    foreach($tournaments as $tournament){
+        $tournament->setActive(0);
+        $tournament->save();
+    }
+
     //Insert un nouveau tournoi
     $tournament = new Tournament();
     $tournament->setActive(true);
